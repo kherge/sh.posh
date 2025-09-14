abc22d3e0668cc22151b0e60250c24f654b84871
Kevin Herrera <kevin@herrera.io>
2025-09-14 19:40:51 +0000

    Adding test scripts.

---
+++ test/debug.sh
@@ -0,0 +1,21 @@
+#!/bin/dash
+
+# This script assumes your current working directory is the root of the
+# project. It will set up POSH to run in DASH, run your command against
+# POSH, and then exit.
+
+# shellcheck disable=SC1091
+# shellcheck disable=SC2155
+
+# Configure the environment.
+export POSH_DIR="$PWD"
+export POSH_DEBUG=1
+
+# Install POSH.
+. ./posh.sh && __posh_init
+
+# Run the desired command.
+posh "$@"
+
+# Preserve exit status.
+exit $?
